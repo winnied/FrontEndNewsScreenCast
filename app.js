9db87b466266b7db811ce7f7e23a0b0e6f0798f7
Winnie <winniedinh89@gmail.com>
2016-04-03 22:18:37 -0700

    added form toggle to js and form styles

--- app.js
+++ app.js
@@ -3,10 +3,16 @@
 //-----------------------
 
 $(document).ready(function(){
+	//on click event that causes blinking 
 	$('.js-like').on('click', function(event){
 		event.preventDefault();
 		$(this).text('Liked')
 			.closest('.news-item')
 			.addClass('is-liked');
+	});
+
+	$('.js-add-link').on('click', function(event){
+		event.preventDefault();
+		$('.js-form').toggleClass('is-visible');
 	});
 });
