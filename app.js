72eccb9dd54874e3bab8de9e132698f9e6982d57
Winnie <winniedinh89@gmail.com>
2016-04-03 21:56:21 -0700

    added blink animation to css and js to add class to DOM object

---
+++ app.js
@@ -0,0 +1,12 @@
+//-----------------------
+// DOCUMENT READY
+//-----------------------
+
+$(document).ready(function(){
+	$('.js-like').on('click', function(event){
+		event.preventDefault();
+		$(this).text('Liked')
+			.closest('.news-item')
+			.addClass('is-liked');
+	});
+});
